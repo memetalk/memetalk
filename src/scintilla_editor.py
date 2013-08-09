a84a6b358fd5ea68f5afee315182da821e46f032
Thiago Silva <thiago@metareload.com>
2013-08-08 22:14:23 -0300

    Debugger: ensuring the cursor is visible when stepping

--- src/scintilla_editor.py
+++ src/scintilla_editor.py
@@ -75,6 +75,7 @@ class SimpleEditor(QsciScintilla):
         self.markerAdd(start_line, self.CURRENT_LINE_MARKER)
         # highlight expression
         self.fillIndicatorRange(start_line,start_col,end_line,end_col,self.CURRENT_RANGE_IND)
+        self.ensureLineVisible(start_line)
 
     def on_margin_clicked(self, nmargin, nline, modifiers):
         print str(nmargin) + ":" + str(nline) + ":" + str(modifiers)
