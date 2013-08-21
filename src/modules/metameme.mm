b139a8725622efeceed122c68032f3960ccce2e7
Thiago Silva <thiago@metareload.com>
2013-08-21 04:45:58 -0300

    adding ometa module

---
+++ src/modules/metameme.mm
@@ -0,0 +1,277 @@
+.license
+.endlicense
+
+.preamble(io)
+  io : memetalk/io/1.0();
+
+.code
+
+// -- module functions --
+
+main: fun() {
+  //ometa { rulename = 10; }
+  var ometa = Base.new("  ometa { haha = 10; }");
+  ometa.apply_with_args("token", "ometa");
+  ometa.apply_with_args("token", "{");
+  //id
+  var idseq = ometa.many1(fun() {
+    ometa.apply("letter_or_digit");
+  });
+  ometa.apply("spaces");
+  ometa.apply("token", "=");
+  var num = ometa.many(fun() {
+    ometa.apply("digit");
+  });
+}
+
+// -- module classes --
+
+class Base
+fields: input;
+init new: fun(data) {
+  @input = Input.new(data);
+}
+
+instance_method apply: fun(rule_name) {
+  //TODO: memo it
+  return this.send(("rule_" + rule_name).toSymbol, []);
+}
+
+instance_method apply_with_args: fun(rule, *rest) {
+  @input = @input.prepend(rest.reversed);
+  this.apply(rule);
+}
+
+instance_method end: fun() {
+  return this.not(fun() { this.apply("anything") });
+}
+
+instance_method form: fun(fn) {
+  var seq = this.apply("anything");
+  if (!is_sequence(seq)) {
+    Fail.throw();
+  }
+  var input = @input;
+  @input = Input.new(seq);
+  var ret = fn();
+  this.apply("end");
+  @input = input;
+  return ret;
+}
+
+instance_method lookahead: fun(fn) {
+  var input = @input;
+  var r = fn();
+  @input = input;
+  return r;
+}
+
+instance_method many: fun(fn, *opt) {
+  var ret = opt;
+
+  while (true) {
+    var input = @input;
+    try {
+      ret = ret + [fn()];
+    } catch(ex) {
+      if (ex.type != Fail) {
+        ex.rethrow();
+      } else {
+        @input = input;
+        return ret;
+      }
+    }
+  }
+}
+
+instance_method many1: fun(fn) {
+  return this.many(fn, fn());
+}
+
+instance_method not: fun(fn) {
+  var input = @input;
+  try {
+    fn();
+  } catch(e) {
+    @input = input;
+    return true;
+  }
+  Fail.throw();
+}
+
+instance_method opt: fun(fn) {
+  var input = @input;
+  var ret = null;
+  try {
+    ret = fn();
+  } catch(ex) {
+    if (ex.type == Fail) {
+      @input = input;
+    }
+  }
+  return ret;
+}
+
+instance_method or: fun(fns) {
+  var input = @input;
+  fns.each(fun(fn) {
+    try { //cl-ometa checks if fn == null, don't remember why
+      @input = input;
+      return fn();
+    } catch(ex) {
+      if (ex.type == Exception) { //not an ometa match-fail
+        e.rethrow();
+      }
+    }
+  });
+}
+
+instance_method pred: fun(x) {
+  if (x) {
+    return true;
+  }
+  Fail.throw();
+}
+
+instance_method rule_anything: fun() {
+  var r = @input.first;
+  @input = @input.rest;
+  return r;
+}
+
+instance_method rule_apply: fun() {
+  var rule = this.apply("anything");
+  return this.apply(rule);
+}
+
+instance_method rule_char: fun() {
+  var r = this.apply("anything");
+  this.pred(Mirror.vtFor(r) == String and r.size() == 1);
+  return r;
+}
+
+instance_method rule_digit: fun() {
+  var r = this.apply("char");
+  this.pred(r >= "0" and r <= "9");
+  return r;
+}
+
+instance_method rule_exactly: fun() {
+  var wanted = this.apply("anything");
+  if (wanted == this.apply("anything")) {
+    return wanted;
+  } else {
+    Fail.throw;
+  }
+}
+
+instance_method rule_false: fun() {
+  var r = this.apply("anything");
+  this.pred(r == false);
+  return r;
+}
+
+instance_method rule_first_and_rest: fun() {
+  var first = this.apply("anything");
+  var rest = this.apply("anything");
+  return this.many(fun() { this.apply(rest) }, this.apply(first));
+}
+
+instance_method rule_letter: fun() {
+  return this.or([fun() {
+    this.apply("lower");
+  }, fun() {
+    this.apply("upper");
+  }]);
+}
+
+instance_method rule_letter_or_digit: fun() {
+  return this.or([fun() {
+    this.apply("letter");
+  }, fun() {
+    this.apply("digit");
+  }]);
+}
+
+instance_method rule_lower: fun() {
+  var r = this.apply("char");
+  this.pred(r.charCode >= "a".charCode and r.charCode <= "z".charCode);
+  return r;
+}
+
+instance_method rule_number: fun() {
+  var r = this.apply("anything");
+  this.pred(Mirror.vtFor(r) == Number);
+  return r;
+}
+
+instance_method rule_seq: fun() {
+  var xs = this.apply("anything");
+  xs.each(fun(x) {
+    this.apply_with_args("exactly", x);
+  });
+  return xs;
+}
+
+instance_method rule_space: fun() {
+  var r = this.apply("char");
+  this.pred(r.charCode <= 32);
+  return r;
+}
+
+instance_method rule_spaces: fun() {
+  return this.many(fun() { this.apply("space") });
+}
+
+instance_method rule_string: fun() {
+  var r = this.apply("anything");
+  this.pred(Mirror.vtFor(r) == String);
+  return r;
+}
+
+instance_method rule_token: fun() { // dummy/stand-in code. I'm hungry, be back latter
+  var tk = this.apply("anything");
+  this.apply("spaces");
+  var found = this.apply_with_args("first_and_rest", "letter_or_digit", "letter");
+  this.pred(tk == found);
+  return tk;
+}
+
+instance_method rule_true: fun() {
+  var r = this.apply("anything");
+  this.pred(r == true);
+  return r;
+}
+
+instance_method rule_upper: fun() {
+  var r = this.apply("char");
+  this.pred(r >= "A" and r <= "Z");
+  return r;
+}
+
+end //metameme:Base
+
+class Fail < Exception
+fields: ;
+init new: fun(msg) { super.new(msg); }
+
+class_method throw: fun() {
+  this.new("Meta-Meme error").throw;
+}
+
+end //metameme:Fail
+
+class Input < List
+fields: ;
+init new: fun(data) {
+  super.new(data);
+}
+
+instance_method prepend: fun(other) {
+  return Input.new(super(other));
+}
+
+end //metameme:Input
+
+
+.end
