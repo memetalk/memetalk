149fd9f0b951038c1686108e5cccecccf9deb592
Thiago Silva <thiago@metareload.com>
2014-06-11 00:04:22 -0400

    CoreCompiler prototype emitting obj/dict instances.
    ...and almost string instances.

--- sugarfoot/core_compiler/compiler.py
+++ sugarfoot/core_compiler/compiler.py
@@ -1,31 +1,21 @@
 # -Make all "dict" point to a dict instance
 # -For each entry so far, add the slots that have literals as value
+# -write disassembler of core to debug it.
 # -do functions for objects
 # -do methods
 
 from parser import MemeParser
 from coretr import CoreTr
 from astbuilder import *
-import pprint
+import etable
+import math
+from pprint import pprint as P
+from pdb import set_trace as br
+
 
 class Entry(object):
-    def __init__(self):
-        pass
-
-class ClassEntry(Entry):
-    def __init__(self, name, super_class):
-        self.name = name
-        self.super_class = super_class
-        if super_class != 'Object':
-            raise Exception('TODO')
-
-    def fill(self, vt):
-        vt.append_new_addr(self.name + 'Behavior') # _vt
-        vt.append_null_value() # _delegate
-        vt.append_new_addr(self.super_class) # parent
-        vt.append_null_value() # dict
-        vt.append_new_addr(self.name + '_CompiledClass') # compiled_class
-
+    def fill(self, etable):
+        raise Exception("Implement me")
 
 class ObjectEntry(Entry):
     def __init__(self, name):
@@ -38,60 +28,115 @@ class ObjectEntry(Entry):
     def add_slot_literal_null(self, name):
         self.slots.append({'type': 'null', 'name': name})
 
-    def fill(self, vt):
-        for slot in self.slots:
-            if slot['type'] == 'ref':
-                vt.append_new_addr(slot['value'])
-            elif slot['type'] == 'null':
-                vt.append_null_value()
-            else:
-                raise Exception('Not implemented')
+    def fill(self, etable):
+        self.emit_slot(etable, self.slots[0], self.name)
+        for slot in self.slots[1:]:
+            self.emit_slot(etable, slot)
 
-class VirtualAddressTable(object):
-    def __init__(self, base):
-        self.obj_table = []
-        self.base = base
-        self.index = {}
-        self.addr_table = []
+    def emit_slot(self, etable, slot, label=None):
+        if slot['type'] == 'ref':
+            return etable.append_label_ref(slot['value'], label)
+        elif slot['type'] == 'null':
+            return etable.append_int(0)
+        else:
+            raise Exception('TODO')
 
-    def address_table(self):
-        return self.addr_table
 
-    def object_table(self):
-        return [x() for x in self.obj_table]
+class ClassEntry(Entry):
+    def __init__(self, name, super_class):
+        self.name = name
+        self.super_class = super_class
+        if super_class != 'Object':
+            raise Exception('TODO')
 
-    def addr_table_ptr(self):
-        return self.base
+    def fill(self, etable):
+        oop_dict = append_empty_dict(etable)
+        delegate = append_object_instance(etable)
 
-    def assign(self, name):
-        self.index[name] = self.base
+        etable.append_label_ref(self.name + 'Behavior', self.name) # vt
+        etable.append_pointer_to(delegate)                         # delegate
+        etable.append_label_ref(self.super_class)                  # parent
+        etable.append_pointer_to(oop_dict)                         # dict
+        etable.append_label_ref(self.name + "_CompiledClass")      # compiled_class
 
-    def append_new_addr(self, name):
-        vaddr = lambda: self.index[name]
-        self.obj_table.append(vaddr)
-        self.addr_table.append(self.base)
-        self.base += 1
 
-    def append_null_value(self):
-        vaddr = lambda: 0
-        self.obj_table.append(vaddr)
-        self.base += 1
+class BehaviorEntry(Entry):
+    def __init__(self, name, parent_name, dictionary):
+        self.name = name + 'Behavior'
+        self.parent_name = parent_name
+        self.dictionary = dictionary
 
+    def fill(self, etable):
+        delegate = append_object_instance(etable)
+        oop_dict = append_empty_dict(etable)
+        etable.append_label_ref('Behavior', self.name)         # vt
+        etable.append_pointer_to(delegate)                     # delegate
+        etable.append_label_ref(self.parent_name + "Behavior") # parent
+        etable.append_pointer_to(oop_dict)                     # dict
+
+class CompiledClassEntry(Entry):
+    def __init__(self, name):
+        self.name = name + '_CompiledClass'
+
+    def fill(self, etable):
+        etable.append_label_ref('CompiledClass', self.name) # vt
+        etable.append_int(0)                                # delegate
+
+
+## instance entries
+
+# def append_object_behavior_instance(etable):
+#     delegate = create_object_instance()
+#     oop = etable.append_label_ref('Object') # vt
+#     etable.append_int(delegate)             # delegate
+#     return oop
+
+def append_object_instance(etable):
+    oop = etable.append_label_ref('Object') # vt
+    etable.append_int(0)                    # delegate: end of chain of delegation
+    return oop
+
+
+def append_string_instance(etable, string):
+    delegate = append_object_instance(etable) # Assumed to be object. In the future, deduce from source code and create appropriate delegate chain
+    oop = etable.append_label_ref('String')   # vt
+    etable.append_int(delegate)                # delegate
+    etable.append_string(string)
+    return oop
+
+def append_empty_dict(etable):
+    delegate = append_object_instance(etable)   # Assumed to be object. In the future, deduce from source code and create appropriate delegate chain
+    oop = etable.append_label_ref('Dictionary') # vt
+    etable.append_pointer_to(delegate)          # delegate
+    etable.append_int(0)                        # dict length
+    return oop
+
+# class DictInstanceEntry(Entry):
+#     def __init__(self, d):
+#         self.d = d
+
+#     def fill(self, etable):
+#         if len(self.d) > 0:
+#             raise Exception('Double check if this works')
+
+#         keyvals_oops = []
+#         for key, val in self.d.iteritems():
+#             keyvals_oops.append(create_string_instance(key))
+#             keyvals_oops.append(val)
+
+#         oop = etable.append_label_ref('Dictionary')
+#         delegate = ObjectInstanceEntry().fill(etable)
+#         etable.append_pointer_to(delegate)
+#         etable.append_int(len(self.d))
+#         for oop in keyvals_oops:
+#             etable.append_pointer_to(oop)
 
 class Compiler(ASTBuilder):
     def __init__(self):
-        self.primes_names = [
-            'Behavior',
-            'ObjectBehavior',
-            'Object_CompiledClass',
-            'Object',
-            'CompiledClass',
-            'CompiledFunction']
-
-        self.primes = {}
-
-        self.other_entries = []
+        self.entries = []
         self.current_object = None
+
+        self.etable = etable.VirtualEntryTable()
 
     def compile(self):
         self.line_offset = 0
@@ -108,32 +153,17 @@ class Compiler(ASTBuilder):
 
     def register_object(self, name):
         self.current_object = ObjectEntry(name)
-        if name in self.primes_names:
-            self.primes[name] = self.current_object
-        else:
-            self.other_entries.append(self.current_object)
+        self.entries.append(self.current_object)
 
     def register_class(self, name, super_class):
         self.current_class = ClassEntry(name, super_class)
-        if name in self.primes_names:
-            self.primes[name] = self.current_class
-        else:
-            self.other_entries.append(self.current_class)
+        self.entries.append(self.current_class)
 
         # registering the behavior for that class
-        behavior = ObjectEntry(name+"Behavior")
-        behavior.add_slot_ref('_vt', 'Behavior')
-        behavior.add_slot_literal_null('_delegate')
-        behavior.add_slot_ref('parent', super_class+'Behavior')
-        behavior.add_slot_literal_null('dict') # TODO: should be dict literal with class methods
-        self.other_entries.append(behavior)
+        self.entries.append(BehaviorEntry(name, super_class, None))
 
         # registering the compiled class for that class
-        cclass = ObjectEntry(name+"_CompiledClass")
-        cclass.add_slot_ref('_vt', 'CompiledClass')
-        cclass.add_slot_literal_null('_delegate')
-        # TODO: fields, methods, own_methods, module, name, super_class_name
-        self.other_entries.append(cclass)
+        self.entries.append(CompiledClassEntry(name))
 
     def add_class_fields(self, fields):
         if len(fields) != 0:
@@ -149,46 +179,46 @@ class Compiler(ASTBuilder):
     ###########
 
     def dump(self):
-        HEADER_SIZE = 2 # index_size, addr_table
+        HEADER_SIZE = 3 * 4 # bytes. 3 = names_size, entries, addr_table_offset
 
         core = {
             'header': {
-                'index_size': None,
-                'addr_table_ptr': None},
+                'names_size': None,          # size in bytes of NAMES section
+                'entries': None,             # number of top level objects
+                'ot_size': None},            # size of OBJECT TABLE in bytes
+            'names': [],
             'index': [],
             'object_table': [],
             'addr_table': []}
 
-        core['header']['index_size'] = len(self.primes_names)
+        # -- HEADER and NAMES section
+        core['header']['entries'] = len(self.entries)
 
-        vt = VirtualAddressTable(HEADER_SIZE + core['header']['index_size'])
+        # names :: [(string, alloc-size in bytes)]
+        core['names'] = [(entry.name, int(math.ceil((len(entry.name) + 1) * 8 / 32.0)) * 4) for entry in self.entries] # +1: "\0"
+        core['header']['names_size'] = sum([x[1] for x in core['names']])
+        core['header']['entries'] = len(self.entries)
 
-        # --- object table
-        # 1) primes
-        for name in self.primes_names:
-            if name not in self.primes:
-                # raise Exception('core source is incomplete: missing prime {}'.format(name))
-                continue
-            vt.assign(name)
-            self.primes[name].fill(vt)
+        # - INDEX section
 
-        # 2) others
-        for entry in self.other_entries:
-            vt.assign(entry.name)
-            entry.fill(vt)
+        index_size = core['header']['entries'] * 2 * 4 # *2: pair (name, entry), *4: bytes
+        base = HEADER_SIZE + core['header']['names_size'] + index_size
+        # self.etable.set_base(100) # debuging
+        self.etable.set_base(base)
 
-        # end of computations
+        # - OBJECT TABLE
+        for entry in self.entries:
+            entry.fill(self.etable)
 
-        core['addr_table'] = vt.address_table()
-        core['object_table'] = vt.object_table()
-        core['header']['addr_table_ptr'] = vt.addr_table_ptr()
+        core['object_table'] = self.etable.object_table()
 
-        self.print_core(core)
+        # -- HEADER ot_size
+        core['header']['ot_size'] = len(core['object_table']) * 4
 
-    def print_core(self, core):
-        pprint.pprint(core)
-        print '---'
-        for idx, x in enumerate(core['object_table']):
-            print '[{}] - {}'.format(idx+8, x)
+        core['addr_table'] =self.etable.addr_table()
+
+        br()
+        for idx, val in enumerate(core['object_table']):
+            print idx + 100, ':', val
 
 Compiler().compile()
--- sugarfoot/core_compiler/etable.py
+++ sugarfoot/core_compiler/etable.py
@@ -1,10 +1,8 @@
-
-
 class Cell(object):
     def index(self):
         return self.etable.base + self.etable.entries.index(self)
 
-class ValueCell(Cell):
+class IntCell(Cell):
     def __init__(self, etable, num):
         self.etable = etable
         self.num = num
@@ -12,13 +10,35 @@ class ValueCell(Cell):
     def __call__(self):
         return self.num
 
-class PointerCell(Cell):
-    def __init__(self, etable, target_cell):
+class StringCell(Cell):
+    def __init__(self, etable, string):
         self.etable = etable
-        self.target_cell = target_cell
+        self.string = string
 
     def __call__(self):
-        return self.etable.base + self.etable.entries.index(self.target_cell)
+        chars = [c for c in self.string] + [0] # \0
+        chars += (len(chars) % 8) * [0]        # padding
+        return chars
+
+class PointerCell(Cell):
+    def __init__(self, etable, label=None, cell=None):
+        self.etable = etable
+        self.target_label = None
+        self.target_cell = None
+
+        if label:
+            self.target_label = label
+        if cell:
+            self.target_cell = cell
+
+        if label is None and cell is None:
+            raise Exception('label or cell required')
+
+    def __call__(self):
+        if self.target_cell:
+            return self.etable.base + self.etable.entries.index(self.target_cell)
+        else:
+            return self.etable.base + self.etable.index[self.target_label]
 
 class VirtualEntryTable(object):
     def __init__(self):
@@ -26,43 +46,65 @@ class VirtualEntryTable(object):
         self.base = 0
         self.index = {}
 
-    def index_size(self):
-        return len(self.index)
-
     def append_int(self, num, label=None):
-        cell = ValueCell(self, num)
+        cell = IntCell(self, num)
+        if label is not None:
+            self.label_current(label)
+        self.entries.append(cell)
+        return cell
+
+    def append_string(self, string, label=None):
+        cell = StringCell(self, string)
         if label is not None:
             self.label_current(label)
         self.entries.append(cell)
         return cell
 
     def append_pointer_to(self, cell, label=None):
-        cell = PointerCell(self, cell)
+        cell = PointerCell(self, cell=cell)
         if label is not None:
             self.label_current(label)
         self.entries.append(cell)
         return cell
 
     def append_label_ref(self, target_label, label=None):
-        if target_label == label:
-            cell = self.circular_cell(label)
-        else:
-            cell = PointerCell(self, self.entries[self.index[target_label]])
+        # if target_label == label:
+        #     cell = PointerCell(self, label)
+        #     cell = self.circular_cell(label)
+        # else:
+        #     print target_label
+        #     br()
+        cell = PointerCell(self, label=target_label)
         if label is not None:
             self.label_current(label)
         self.entries.append(cell)
         return cell
 
-    def circular_cell(self, label):
-        cell = PointerCell(self, None)
-        cell.target_cell = cell
-        return cell
+    # def circular_cell(self, label):
+    #     cell = PointerCell(self, label)
+    #     return cell
 
     def set_base(self, base):
         self.base = base
 
     def label_current(self, label):
         self.index[label] = len(self.entries)
+
+    ###
+
+    def addr_table(self):
+        return [self.base + idx for idx, x in enumerate(self.entries) if type(x) == PointerCell]
+
+    def object_table(self):
+        ot = [x() for x in self.entries]
+        # TODO: expand variable sized cells (eg. string) updating the references
+        #  ie. some items in ot are not 32 bit values, but arrays of variable length.
+        #      they should be flattened with ot and all further references from that point should be updated.
+        #      for value in ot:
+        #        if type(value) == list:
+        #            ot = ot[:here] + value + ot[here:]
+        #            update_refs(len(value) ot[here:])
+        return ot
 
     def dump(self):
         for idx, x in enumerate(self.entries):
@@ -76,5 +118,7 @@ if __name__ == '__main__':
     c3 = tb.append_label_ref('Object')
     c4 = tb.append_int(30)
     c5 = tb.append_pointer_to(c3)
+    c5 = tb.append_label_ref('Foo') # forward reference
+    c7 = tb.append_int(40, 'Foo')
     tb.set_base(100)
     tb.dump()
