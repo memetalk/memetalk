d3f2af3b9274d441afadaceae25ab8781d97b4ff
Thiago Silva <thiago@metareload.com>
2014-07-28 15:06:56 -0400

    Add extra closure tests

---
+++ sugarfoot/tests/closures27.mm
@@ -0,0 +1,23 @@
+.preamble(test)
+.code
+
+main: fun() {
+  var f1 = fun() {
+    var z = 0;
+    return fun(k) { z = z + k; z };
+  };
+  var f2 = fun() {
+    var z = 0;
+    return fun(k) { z = z + k; z };
+  };
+
+  var f3 = f1();
+  var f4 = f2();
+  test.assert(f3(10) == 10, "local f1::z receives 10");
+  test.assert(f3(10) == 20, "local f1::z receives 20");
+
+  test.assert(f4(10) == 10, "local f2::z receives 10");
+  test.assert(f4(10) == 20, "local f2::z receives 20");
+}
+
+.end
---
+++ sugarfoot/tests/closures5.mm
@@ -0,0 +1,31 @@
+.license
+.endlicense
+
+.preamble(test)
+.code
+
+// -- module functions --
+
+main: fun() {
+  var z = 9;
+  var f = fun() {
+    var y = 10;
+    return fun(x) { z + x + y };
+  };
+  var w = fun() {
+    var z = 1000;
+    return z;
+  };
+  var j = w();
+  var k = x(f);
+  test.assert(x(f) == 29, "Shadowing identifier with nested closures");
+}
+
+x: fun(g) {
+  return g()(10);
+}
+
+// -- module classes --
+
+
+.end
