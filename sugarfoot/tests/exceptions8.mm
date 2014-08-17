dc97c6cc38ad74879c98040c1d055331c667997f
Thiago Silva <thiago@metareload.com>
2014-08-16 20:41:39 -0400

    Adding test for unwind exceptions with primitives

---
+++ sugarfoot/tests/exceptions8.mm
@@ -0,0 +1,26 @@
+.license
+.endlicense
+
+.preamble(test)
+
+.code
+
+baz: fun() {
+  Exception.throw("baz_ex");
+  test.assert(false, "should not execute this");
+}
+
+bar: fun() {
+  baz();
+  test.assert(false, "should not execute this");
+}
+
+foo: fun() {
+  <primitive "test_catch_exception">
+}
+
+main: fun() {
+  test.assert(foo().message == "baz_ex", "Primitive handled exception");
+}
+
+.end
