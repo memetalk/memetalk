fd1f006819669f243d50eedb564c832160c80a95
Thiago Silva <thiago@metareload.com>
2016-06-15 18:25:56 -0400

    Inlining utils functions

--- sugarfoot/utils.cpp
+++ sugarfoot/utils.cpp
@@ -93,48 +93,7 @@ void link_symbols(char* data, int es_size, int start_external_symbols, VM* vm, C
   }
 }
 
-int decode_opcode(bytecode code) {
-  return (0xFF000000 & code) >> 24;
-}
 
-int decode_args(bytecode code) {
-  return 0xFFFFFF & code;
-}
-
-
-
-bool is_small_int(oop num) {
-#if WSIZE == 8
-  return (bool) ((word) num & 0x8000000000000000);
-#else
-  return (bool) ((word) num & 0x80000000);
-#endif
-}
-
-number untag_small_int(oop num) {
-  word w = (word) num;
-#if WSIZE == 8
-  if (w & 0x4000000000000000) { //Negative
-    return (w & 0x7FFFFFFFFFFFFFFF) | 0x8000000000000000;
-  } else {
-    return w & 0x7FFFFFFFFFFFFFFF;
-  }
-#else
-  if (num & 0x40000000) { //Negative
-    return (w & 0x7FFFFFFF) | 0x80000000;
-  } else {
-    return (w & 0x7FFFFFFF;
-  }
-#endif
-}
-
-oop tag_small_int(number num) {
-#if WSIZE == 8
-  return (oop) (num | 0x8000000000000000);
-#else
-  return (oop) (num | 0x80000000);
-#endif
-}
 
 
 bool check_and_print_exception(Process* proc, int exc, oop ex) {
--- sugarfoot/utils.hpp
+++ sugarfoot/utils.hpp
@@ -14,14 +14,48 @@ word unpack_word(const char* data, int offset);
 void relocate_addresses(char* data, int data_size, int start_reloc_table);
 void link_symbols(char* data, int es_size, int start_external_symbols, VM* vm, CoreImage* core);
 
-int decode_opcode(bytecode);
-int decode_args(bytecode);
+inline int decode_opcode(bytecode code) {
+  return (0xFF000000 & code) >> 24;
+}
+
+inline int decode_args(bytecode code) {
+  return 0xFFFFFF & code;
+}
 
 std::string bytecode_to_str(bytecode code);
 
-bool is_small_int(oop num);
-number untag_small_int(oop num);
-oop tag_small_int(number num);
+inline bool is_small_int(oop num) {
+#if WSIZE == 8
+  return (bool) ((word) num & 0x8000000000000000);
+#else
+  return (bool) ((word) num & 0x80000000);
+#endif
+}
+
+inline number untag_small_int(oop num) {
+  word w = (word) num;
+#if WSIZE == 8
+  if (w & 0x4000000000000000) { //Negative
+    return (w & 0x7FFFFFFFFFFFFFFF) | 0x8000000000000000;
+  } else {
+    return w & 0x7FFFFFFFFFFFFFFF;
+  }
+#else
+  if (num & 0x40000000) { //Negative
+    return (w & 0x7FFFFFFF) | 0x80000000;
+  } else {
+    return (w & 0x7FFFFFFF;
+  }
+#endif
+}
+
+inline oop tag_small_int(number num) {
+#if WSIZE == 8
+  return (oop) (num | 0x8000000000000000);
+#else
+  return (oop) (num | 0x80000000);
+#endif
+}
 
 bool check_and_print_exception(Process* proc, int exc, oop ex);
 
