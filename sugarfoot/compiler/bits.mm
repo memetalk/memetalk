13ffe1bd51d5ebc6072a6953ef37e59c9b090cfa
Thiago Silva <thiago@metareload.com>
2017-08-25 16:06:23 -0400

    Porting bits.mm

--- sugarfoot/compiler/bits.mm
+++ sugarfoot/compiler/bits.mm
@@ -4,6 +4,8 @@
  [Test] <= memetest;
 .code
 
+WSIZE: 8;
+
 class FormatException < Exception
 class_method check: fun(pred, message) {
   if (!pred) {
@@ -22,7 +24,6 @@ unpack: fun(str) {
 }
 
 pack: fun(num) {
-  //TODO: check range of unsigned num (0 < num < 18446744073709551616L)?
   var res = "";
   [0,1,2,3,4,5,6,7].each(fun(_, i) {
     var n = (num >> (i * 8)) & 0xFF;
@@ -31,6 +32,33 @@ pack: fun(num) {
   return res;
 }
 
+pack_byte: fun(num) {
+    return num.asChar;
+}
+
+untag: fun(num) {
+  var flag = (4611686018427387903 * 2) + 1; //0x7FFFFFFFFFFFFFFF
+  return num & flag;
+}
+
+tag: fun(num) {
+  var flag = (-4611686018427387903 - 1) * 2; //0x8000000000000000
+  return num | flag;
+}
+
+bytelist: fun(num) {
+  return pack(num).map(fun(x) { x.toByte });
+}
+
+
+bytelist_tag: fun(num) {
+  return pack(tag(num)).map(fun(x) { x.toByte });
+}
+
+string_block_size: fun(string) {
+  //number of bytes required for string, aligned to word size
+  return (string.size / WSIZE).ceil * WSIZE;
+}
 
 
 // tests
@@ -41,6 +69,13 @@ main: fun() {
 
   var s = pack(0x102030405060708);
   t.assertEqual(s, "\x08\x07\x06\x05\x04\x03\x02\x01", "pack");
+
+  t.assertEqual(untag(tag(10)), 10, "tagging");
+
+  t.assertEqual(bytelist(1010), [242, 3, 0, 0, 0, 0, 0, 0], "bytelist");
+  t.assertEqual(bytelist_tag(1010), [242, 3, 0, 0, 0, 0, 0, 128], "bytelist_tag");
+  t.assertEqual(string_block_size   ("1"), WSIZE, "string_block_size 1");
+  t.assertEqual(string_block_size   ("123456789"), WSIZE * 2, "string_block_size 1");
 }
 
 .endcode
